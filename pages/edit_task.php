81c81b0250824672f6ae3380ea9fb70d3897be62
mashaelym <ma735@njit.edu>
2017-12-14 09:41:35 -0500

    changes in register pages

--- pages/edit_task.php
+++ pages/edit_task.php
@@ -3,4 +3,3 @@ put a page with in a form to edit a task and have a delete button in another for
 <form action="index.php?page=tasks&action=delete&id=<?php $data['id'] ?> " method="post" id="form1">
     <button type="submit" form="form1" value="delete">Delete</button>
 </form>
-
