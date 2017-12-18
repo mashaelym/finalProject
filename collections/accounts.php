3a95c6aa7a5293a49a6324708fc9017fb9d47cbe
mashaelym <ma735@njit.edu>
2017-12-18 10:24:07 -0500

    changes to collections todos and accounts

--- collections/accounts.php
+++ collections/accounts.php
@@ -1,8 +1,33 @@
 <?php
 
-class accounts extends \database\collection
+namespace collection;
+
+use \database\collection;
+
+class accounts extends collection
 {
-    protected static $modelName = 'account';
+	
+	//Define model that will be used to bind the results
+	 
+    protected static $modelName = '\model\account';
+	
+	
+	 //Find User By Email
+	 
+	public static function findUserbyEmail($email)
+    {
+		  $tableName = preg_replace('/.*\\\\/', '', get_called_class());
+		  $sql = 'SELECT * FROM ' . $tableName . ' WHERE email = ?';
+ 
+          //grab the only record for find one and return as an object
+          $recordsSet = self::getResults($sql, self::$modelName, $email);
+ 
+          if (is_null($recordsSet)) {
+               return FALSE;
+          } else {
+               return $recordsSet[0];
+          }
+     }
 }
 
 ?>
--- collections/todos.php
+++ collections/todos.php
@@ -1,8 +1,33 @@
- <?php
+<?php
+namespace collection;
+
+use \database\collection;
  
- class todos extends database\collection
- {
-     protected static $modelName = 'todo';
- }
+class todos extends collection
+{
+	
+	
+	// Model Name
+	 
+	protected static $modelName = '\model\todo';
+	
+	
+	//Find Tasks By User Id
+	 
+	public static function findTasksbyUserId($userId)
+	{
+	  
+        $tableName = preg_replace('/.*\\\\/', '', get_called_class());
+		$sql = 'SELECT * FROM ' . $tableName . ' WHERE ownerid = ?';
+		 //grab the only record for find one and return as an object
+		$recordsSet = self::getResults($sql, self::$modelName, $userId);
+ 
+		 if (is_null($recordsSet)) {
+			 return FALSE;
+		 } else {
+			 return $recordsSet;
+		 }
+	}
+}
  
  ?>
